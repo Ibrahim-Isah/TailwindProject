aff6ce0a4cd0bb1fa173431113f23c71f38d29c2
Isah Ibrahim <isahibn08@gmail.com>
2021-05-10 20:56:38 +0100

    created my first tailwind project without a tutorial

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e6e6ff',
+        second: '#666699',
+        secondary: {
+          100: '#E2E2D5', 
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
